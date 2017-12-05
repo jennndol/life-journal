5f36b7078c6d84250960fd8b9b03d6f58984f1ca
Luthfi <vlootfie@gmail.com>
2017-12-05 15:03:39 +0700

    set routes

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
 
-app.listen(3000);
+/*Routing*/
+const userRouter = require('./routes/user');
+const journalRouter = require('./routes/journal');
+const categoryRouter = require('./routes/category');
+
+app.set('view engine', 'ejs');
+app.use('/static', express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false })) 
+app.use(bodyParser.json())
+
+app.listen(3000, () => console.log('School app listening on port 3000!'));
