cb35d27c719e4ebc6317e27ac15a847420f30360
Luthfi <vlootfie@gmail.com>
2017-12-05 20:09:02 +0700

    sign up feature

--- routes/user.js
+++ routes/user.js
@@ -3,14 +3,33 @@ const router = express.Router();
 const Model = require('../models')
 
 router.get('/', (req, res) => {
-    Model.User.findAll().then((users)=>{
-    	res.render('users/index', {
-    		title : 'Users List',
-    		users : users,
-    	})
-    }).catch((err)=>{
-    	console.log(err);
+    Model.User.findAll().then((users) => {
+        res.render('users/index', {
+            title: 'Users List',
+            users: users,
+        })
+    }).catch((err) => {
+        console.log(err);
     })
+});
+
+router.get('/signup', (req, res) => {
+    res.render('users/signup', {
+        title: 'Sign Up'
+    })
+});
+
+router.post('/signup', (req, res)=>{
+	Model.User.create({
+		username : req.body.username,
+		password : req.body.password
+	})
+	.then(()=>{
+		res.send('SUDAH TERDAFTAR');
+	})
+	.catch(error => {
+		res.send(error);
+	});
 });
 
 module.exports = router;
