825028a158aaa89f00e07891a83610d97e1bfa59
Luthfi <vlootfie@gmail.com>
2017-12-05 14:21:55 +0700

    add seeder for User and Category

--- seeders/20171205071219-user.js
+++ seeders/20171205071219-user.js
@@ -1,26 +1,26 @@
 'use strict';
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.bulkInsert('User', [{
+            username: 'johndoe',
+            password: '123456',
+        }, {
+            username: 'agus',
+            password: '123456',
+        }, {
+            username: 'mayang',
+            password: '123456',
+        }, {
+            username: 'username',
+            password: 'password',
+        }, {
+            username: 'lennon',
+            password: '123456',
+        }, ], {});
+    },
 
-      Example:
-      return queryInterface.bulkInsert('Person', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
-  },
-
-  down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('Person', null, {});
-    */
-  }
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.bulkDelete('User', null, {});
+    }
 };
